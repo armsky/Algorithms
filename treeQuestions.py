7c597229605da785c1cb1c905a08f349e8247f7d
armsky <armsky1988@gmail.com>
2015-03-23 22:39:47 -0400

    add some new lines

--- treeQuestions.py
+++ treeQuestions.py
@@ -1,5 +1,7 @@
 """
-1. Given the root of a binary search tree and 2 numbers min and max, trim the tree such that all the numbers in the new tree are between min and max (inclusive). The resulting tree should still be a valid binary search tree.
+1. Given the root of a binary search tree and 2 numbers min and max, 
+trim the tree such that all the numbers in the new tree are between min and max (inclusive). 
+The resulting tree should still be a valid binary search tree.
 """
 # Remove all nodes bot between in that range by performing a post-order traversal.
 # Must be post-order to ensure it's a bottom-up
