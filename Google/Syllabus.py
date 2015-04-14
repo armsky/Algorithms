559c98ade7ed1cc7ea0f971629ceda6e4b609af6
armsky <armsky1988@gmail.com>
2015-04-13 20:45:05 -0400

    New start for Google

---
+++ Google/Syllabus.py
@@ -0,0 +1,16 @@
+"""
+Syllabus
+1) Dynamic Programming
+2) Super recursion (permutation, combination,...2^n, m^n, n!...etc. type of program. (NP hard, NP programs)
+3) Probability related programs
+4) Graphs: BFS/DFS are usually enough
+5) All basic data structures from Arrays/Lists to circular queues, BSTs, Hash tables, B-Trees, and Red-Black trees, and all basic algorithms like sorting, binary search, median,...
+6) Problem solving ability at a level similar to TopCoder Division 1, 250 points. If you can consistently solve these, then you are almost sure to get in with 2-weeks brush up.
+7) Review all old interview questions in Glassdoor to get a feel. If you can solve 95% of them at home (including coding them up quickly and testing them out in a debugger + editor setup), you are in good shape.
+8) Practice coding--write often and write a lot. If you can think of a solution, you should be able to code it easily...without much thought.
+9) Very good to have for design interview: distributed systems knowledge and practical experience.
+10) Good understanding of basic discrete math, computer architecture, basic math.
+11) Coursera courses and assignments give a lot of what you need to know.
+12) Note that all the above except the first 2 are useful in "real life" programming too!
+"""
+
