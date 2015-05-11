052d62ecf8363057a8242aee55b74f3c100b6a9f
armsky <armsky1988@gmail.com>
2015-05-10 22:05:46 -0400

    more regex

--- Google/regExQuestions.py
+++ Google/regExQuestions.py
@@ -37,4 +37,73 @@ a| b	Matches either a or b.
 \n, \t, etc.	Matches newlines, carriage returns, tabs, etc.
 \1...\9	Matches nth grouped subexpression.
 \10	Matches nth grouped subexpression if it matched already. Otherwise refers to the octal representation of a character code.
+
+Character classes
+Example	Description
+[Pp]ython	Match "Python" or "python"
+rub[ye]	Match "ruby" or "rube"
+[aeiou]	Match any one lowercase vowel
+[0-9]	Match any digit; same as [0123456789]
+[a-z]	Match any lowercase ASCII letter
+[A-Z]	Match any uppercase ASCII letter
+[a-zA-Z0-9]	Match any of the above
+[^aeiou]	Match anything other than a lowercase vowel
+[^0-9]	Match anything other than a digit
+Special Character Classes
+Example	Description
+.	Match any character except newline
+\d	Match a digit: [0-9]
+\D	Match a nondigit: [^0-9]
+\s	Match a whitespace character: [ \t\r\n\f]
+\S	Match nonwhitespace: [^ \t\r\n\f]
+\w	Match a single word character: [A-Za-z0-9_]
+\W	Match a nonword character: [^A-Za-z0-9_]
+Repetition Cases
+Example	Description
+ruby?	Match "rub" or "ruby": the y is optional
+ruby*	Match "rub" plus 0 or more ys
+ruby+	Match "rub" plus 1 or more ys
+\d{3}	Match exactly 3 digits
+\d{3,}	Match 3 or more digits
+\d{3,5}	Match 3, 4, or 5 digits
+Nongreedy repetition
+This matches the smallest number of repetitions ?
+
+Example	Description
+<.*>	Greedy repetition: matches "<python>perl>"
+<.*?>	Nongreedy: matches "<python>" in "<python>perl>"
+Grouping with Parentheses
+Example	Description
+\D\d+	No group: + repeats \d
+(\D\d)+	Grouped: + repeats \D\d pair
+([Pp]ython(, )?)+	Match "Python", "Python, python, python", etc.
+Backreferences
+This matches a previously matched group again ?
+
+Example	Description
+([Pp])ython&\1ails	Match python&pails or Python&Pails
+(['"])[^\1]*\1	Single or double-quoted string. \1 matches whatever the 1st group matched. \2 matches whatever the 2nd group matched, etc.
+Alternatives
+Example	Description
+python|perl	Match "python" or "perl"
+rub(y|le))	Match "ruby" or "ruble"
+Python(!+|\?)	"Python" followed by one or more ! or one ?
+Anchors
+This needs to specify match position.
+
+Example	Description
+^Python	Match "Python" at the start of a string or internal line
+Python$	Match "Python" at the end of a string or line
+\APython	Match "Python" at the start of a string
+Python\Z	Match "Python" at the end of a string
+\bPython\b	Match "Python" at a word boundary
+\brub\B	\B is nonword boundary: match "rub" in "rube" and "ruby" but not alone
+Python(?=!)	Match "Python", if followed by an exclamation point.
+Python(?!!)	Match "Python", if not followed by an exclamation point.
+Special Syntax with Parentheses
+Example	Description
+R(?#comment)	Matches "R". All the rest is a comment
+R(?i)uby	Case-insensitive while matching "uby"
+R(?i:uby)	Same as above
+rub(?:y|le))	Group only without creating \1 backreference
 """
